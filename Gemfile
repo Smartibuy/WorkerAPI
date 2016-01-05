2b81a1c193a40c3d4645f2c6920f7a81e0ca5b11
Katy Wu <katy12032001@gmail.com>
2016-01-05 18:43:46 +0800

    apiserver for parse keyword

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+ruby '1.9.3'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'json'
+gem 'jieba-jruby'
+
+group :test do
+  gem 'rack'
+  gem 'rake'
+  gem 'rack-test'
+  gem 'minitest'
+end
---
+++ application_controller.rb
@@ -0,0 +1,21 @@
+# encoding: UTF-8
+require 'sinatra/base'
+require 'jieba'
+require 'json'
+
+class ApplicationController < Sinatra::Base
+
+  # Web App Views Routes
+  post '/parse_keyword' do
+    content_type :json
+    begin
+      req = JSON.parse(request.body.read)
+    rescue
+      halt 404
+    end
+    keyword = req['keyword']
+    keyword_arr = keyword.to_tags
+    keyword_arr.to_json
+  end
+
+end
